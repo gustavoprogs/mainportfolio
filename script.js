a99151b056bea00ec5c76a5780544fdfc45d96cb
Gustavo <gustavoecl96@gmail.com>
2021-08-26 20:39:05 -0300

    Sessao projetos criada

--- script.js
+++ script.js
@@ -14,4 +14,25 @@ $(document).ready(function(){
     $('.menu-btn i').toggleClass("active");
     });
 
+    // Script do carrosel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 200,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
