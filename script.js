20ad36594bc906c00ad916a1f10503c2cb8762aa
Gustavo <gustavoecl96@gmail.com>
2021-08-29 13:36:44 -0300

    Readme

--- script.js
+++ script.js
@@ -5,6 +5,31 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // Scroll Up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0})
+    });
+
+    // Textos com animação
+    var typed = new Typed(".typing", {
+        strings: ["Front-end;", "Freelancer;", "Junior;", "Criativo!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Front-end;", "Freelancer;", "Junior;", "Criativo!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true
     });
 
     // Criando o Menu Toggle
